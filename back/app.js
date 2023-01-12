7807b32f75e04783fda380594f243ab8ff13a224
Gourson Glenn <114163295+GennGur@users.noreply.github.com>
2023-01-12 14:18:13 +0100

    Structure code back

--- back/app.js
+++ back/app.js
@@ -2,8 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-const Thing = require('./models/thing');
-const thing = require('./models/thing');
+const stuffRoutes = require('./routes/stuff');
 
 mongoose.connect('mongodb+srv://User1:User1password@cluster0.fchxxu2.mongodb.net/?retryWrites=true&w=majority',
   { useNewUrlParser: true,
@@ -22,38 +21,8 @@ app.use((req, res, next) => {
     next();
 });
 
-app.post('/api/stuff', (req, res, next) => {
-    delete req.body._id;
-    const thing = new Thing({
-      ...req.body
-    });
-    thing.save()
-      .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
-      .catch(error => res.status(400).json({ error }));
-});
+app.use(bodyParser.json());
 
-app.put('/api/stuff/:id', (req, res, next) => {
-    Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-      .then(() => res.status(200).json({ message: 'Objet modifié !'}))
-      .catch(error => res.status(400).json({ error }));
-});
-
-app.delete('/api/stuff/:id', (req, res, next) => {
-    Thing.deleteOne({ _id: req.params.id})
-      .then(() => res.status(200).json({ message: 'Objet suprimé !'}))
-      .catch(error => res.status().json({ error }) );
-});
-
-app.get('/api/stuff/:id', (req, res, next) => {
-    Thing.findOne({ _id: req.params.id })
-      .then(thing => res.status(200).json(thing))
-      .catch(error => res.status(404).json({ error }));
-});
-
-app.get('/api/stuff', (req, res, next) => {
-    Thing.find()
-      .then(things => res.status(200).json(things))
-      .catch(error => res.status(400).json({ error }));
-  });
+app.use('/api/stuff', stuffRoutes);
 
 module.exports = app;
